8f3dab1947abc3ed2f7e782599adb8e793069cd9
Levi Bensley <levi@leaneasy.co.uk>
2022-06-27 16:52:46 +0100

    Initial commit

---
+++ le_website_exclude_url/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Remove Sitemap URLS',
+    'version': '1.0.0',
+    'category': 'Website',
+    'summary': 'Allows the removal of specific urls from the sitemap.',
+    'description': """
+        sitemap
+        remove from sitemap
+        exclude url from sitemap
+        exclude url sitemap
+        remove url sitemap
+        sitemap improvements
+        advanced sitemap
+    """,
+    'author': "Lean Easy",
+    'website': "https://www.leaneasy.co.uk",
+    'depends': ['base', 'website'],
+    'data': [
+        'views/res_config_settings.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'OPL-1',
+}
