f618d7f290ff5ecde496138419f64893f32c756c
Levi Bensley <levi@leaneasy.co.uk>
2022-06-27 17:05:50 +0100

    fix field label

--- le_website_exclude_url/models/res_config_settings.py
+++ le_website_exclude_url/models/res_config_settings.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    le_excl_sitemap_url = fields.Text(string="Exclude from URL",related='website_id.le_excl_sitemap_url', readonly=False)
+    le_excl_sitemap_url = fields.Text(string="Exclude from Sitemap",related='website_id.le_excl_sitemap_url', readonly=False)
